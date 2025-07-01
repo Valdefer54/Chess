294fb95690c5c99c81ce399cd8f7882b8e626109
Valdefer54 <127528132+Valdefer54@users.noreply.github.com>
2025-07-01 18:43:21 -0500

    añadimos la clase king

---
+++ pieces/king.py
@@ -0,0 +1,17 @@
+class king:
+    def __init__(self, color, position):
+        self.color = color
+        self.position = position
+        self.name = "king"
+        self.symbol = "♔" if color == "white" else "♚"
+
+    def gmove(self, new_position):
+        x_diff = abs(new_position[0] - self.position[0])
+        y_diff = abs(new_position[1] - self.position[1])
+
+        # King moves one square in any direction
+        if (x_diff <= 1 and y_diff <= 1):
+            self.position = new_position
+            return True
+
+        return False
